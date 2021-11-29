c6b23e96a0c82d3a3c653502245fb9bbd2c9acca
light <light2adrifter@yahoo.com>
2021-11-29 18:52:43 -0500

    google search terminal

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,6 +1,8 @@
 // googleSearchApiKey = AIzaSyBVpEkmXFRFPLuNtirar23cbSNCcOD-2gg
 
-
+// googlesearch terminal Copy the following code, and paste it into a <div> element in your site's <body> section, where you want both of the search box and the search results to render.
+{/* <script async src="https://cse.google.com/cse.js?cx=6f50efadbd4e8bad7"></script> */}
+{/* <div class="gcse-search"></div> */}
 
 
 document.addEventListener('DOMContentLoaded', () => {
